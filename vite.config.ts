4e538fbf0fe582ca531c578fa64d045ad0d252f1
Mabengi <trickson.mabengi@gmail.com>
2025-08-10 11:32:10 +0100

    Fix chemin start pour Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-    host: '0.0.0.0',
-    allowedHosts: ['nzooimmo.com', 'www.nzooimmo.com'],
-  },
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    port: 5173,
     host: '0.0.0.0',
   },
-  base: '/',  // à ajouter si ce n’est pas déjà fait
+  base: '/',
 });
-
-
