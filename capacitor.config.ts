4d03c680056cf1b85c3cdbe704d5ce8fe9fb2c2a
Nzoo Auto <dev@nzoo.immo>
2025-09-19 10:42:13 +0100

    chore: sync local changes and cleanup unused guides/tests

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nzoo.immo',
+  appName: 'Nzoo Immo',
+  webDir: 'dist'
+};
+
+export default config;
